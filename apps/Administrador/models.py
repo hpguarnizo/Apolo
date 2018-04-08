c3ca3a41cdfdd6890ef4812883b99c99631d3fc2
XeresRed <cam960210@gmail.com>
2018-04-08 16:45:54 -0500

    Se añadio la funcionalidad de login y registro administrador

--- apps/Administrador/models.py
+++ apps/Administrador/models.py
@@ -3,9 +3,13 @@ from apps.Usuario.models import Persona
 
 # Create your models here.
 class Administrador(models.Model):
+    SECTOR_CHOICES = (
+        ("1", "Tiene permisos de super administrador"),
+        ("0","No tiene permiso"),
+    )
     Identificacion = models.CharField(primary_key=True, max_length=25)
-    usuario = models.OneToOneField(Persona, on_delete=models.CASCADE)
-    permiso = models.BooleanField(default=False)
+    usuario = models.OneToOneField(Persona, null=True, on_delete=models.CASCADE)
+    permiso = models.CharField(max_length=85,choices=SECTOR_CHOICES,default="1")
 
     def __str__(self):
         return self.Identificacion
--- apps/Administrador/views.py
+++ apps/Administrador/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.urls import reverse
+from apps.Usuario.models import Persona
+from apps.Administrador.models import Administrador
+from apps.Usuario.forms import Registro_Persona_Form
+from apps.Administrador.forms import Registro_Administrador_Form
+from django.contrib.auth import authenticate, login as dj_login
+from django.contrib.auth.decorators import login_required
 
+def login(request):
+    if request.method == "POST":
+        usuario = request.POST['correo']
+        password = request.POST['pass']
+        busqueda = list(Persona.objects.filter(username=usuario))
+        if len(busqueda) > 0:
+            user = authenticate(username=usuario, password=password)
+            persona = Persona.objects.get(username=request.POST["correo"])
+            if user is not None:
+                dj_login(request,user)
+                request.session['pk_admon'] = persona.username
+                return redirect('admon:inicio')
+            else:
+                return render(request,"Home/login.html",{ 'Mensaje':'La contraseña es incorrecta.'})
+        else:
+            return render(request,"Home/login.html",{ 'Mensaje':'El correo electronico ingresado no es valido'})
+
+    return render(request, 'Home/login.html',{ 'Mensaje':''})
+
+@login_required(login_url='admon:registro')
 def index(request):
-    return render(request,"Administrador/index.html")
+    username = request.session['pk_admon']
+    persona = Persona.objects.get(username=username)
+    persona_admin = list(Administrador.objects.filter(usuario=persona))
+    if len(persona_admin) > 0:
+        admin = Administrador.objects.get(usuario=persona)
+        return render(request,"Administrador/index.html",{'admin':admin, 'empresa': ''})
+    else:
+        return redirect('empresa:registro')
+    
 
 def verEvaluacion(request):
     return render(request,"Administrador/verEva.html")
@@ -15,3 +49,42 @@ def verPlan(request):
 
 def crearPlan(request):
     return render(request,"Administrador/crearPlan.html")
+
+def Registro_administrador(request):
+    if request.method == 'POST':
+        formulario_persona = Registro_Persona_Form(request.POST)
+        formulario_admin = Registro_Administrador_Form(request.POST);
+        if(formulario_persona.is_valid() & formulario_admin.is_valid()):
+            if request.POST["password"] == request.POST["c_contraseña"]:
+                #Guardo formulario de registro de usuario
+                formulario = formulario_persona.save(commit=False)
+                formulario_admin_s = formulario_admin.save(commit=False)
+            
+                #cifro contraseña
+                formulario.set_password(formulario_persona.cleaned_data['password'])
+                formulario.email = request.POST["username"]
+                formulario.save()
+
+                formulario_admin_s.usuario = formulario
+                formulario_admin_s.save()
+                return render(request, 'Administrador/registro_admin.html', {'form':formulario_persona, 'form_admin': formulario_admin,'Mensaje':'se ha registrado el usuario con exito.','tipo':'success'})
+            else:
+                return render(request, 'Administrador/registro_admin.html', {'form':formulario_persona, 'form_admin': formulario_admin, 'Mensaje':'Las contraseñas no coinciden.','tipo':'danger'})
+        else:
+            return render(request, 'Administrador/registro_admin.html', {'form':formulario_persona, 'form_admin': formulario_admin, 'Mensaje':'Se tiene un error en el formulario.','tipo':'danger'})
+            
+    else:
+        formulario_persona = Registro_Persona_Form()
+        formulario_admin = Registro_Administrador_Form()
+
+    return render(request, 'Administrador/registro_admin.html', {'form':formulario_persona,'form_admin': formulario_admin, 'Mensaje':'','tipo':'success'})
+
+@login_required(login_url='admon:registro')
+def logout_view(request):
+    #try:
+    #    del request.session['pk_user']
+    #except KeyError:
+    #    pass
+    #causa un error cuando se hace el llamado 
+    django_logout(request)
+    return redirect('admon:registro')
